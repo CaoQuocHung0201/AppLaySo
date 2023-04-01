5764ebf9cc4ff31e8d7ebd34bac78636c3c0b340
QuocHung <quochung5962@gmail.com>
2023-04-01 19:23:56 +0700

    vsdv

--- app/src/main/java/com/hduy/app_lay_so/Controller/PrinterCommands.java
+++
@@ -1,6 +0,0 @@
-package com.hduy.app_lay_so.Controller;
-
-public class PrinterCommands {
-    public static final byte[] ESC_ALIGN_CENTER = new byte[]{0x1B, 0x61, 0x01};
-    public static final byte[] LF = new byte[]{0x0A};
-}
